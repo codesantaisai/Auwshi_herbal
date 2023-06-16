9cd642c278584dc9201dde950fc6c746381f6452
tanolingam <tanolingam09@gmail.com>
2023-06-16 10:38:09 +0530

    dash

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -46,7 +46,7 @@ export default function Dashboard () {
                 <h1 className="my-4">Dashboard</h1>
                 <div className="row pr-4">
                     <div className="col-xl-12 col-sm-12 mb-3">
-                        <div className="card text-white bg-primary o-hidden h-100">
+                        <div className="card1 text-white o-hidden h-100">
                             <div className="card-body">
                                 <div className="text-center card-font-size">Total Amount<br /> <b>LKR {totalAmount}</b>
                                 </div>
@@ -56,7 +56,7 @@ export default function Dashboard () {
                 </div>
                 <div className="row pr-4">
                     <div className="col-xl-3 col-sm-6 mb-3">
-                        <div className="card text-white bg-success o-hidden h-100">
+                        <div className="card text-white o-hidden h-100" style={{ backgroundColor: '#B5927D' }}>
                             <div className="card-body">
                                 <div className="text-center card-font-size">Products<br /> <b>{products.length}</b></div>
                             </div>
@@ -71,7 +71,7 @@ export default function Dashboard () {
 
 
                     <div className="col-xl-3 col-sm-6 mb-3">
-                        <div className="card text-white bg-danger o-hidden h-100">
+                        <div className="card text-white o-hidden h-100"  style={{ backgroundColor: '#B5927D' }} >
                             <div className="card-body">
                                 <div className="text-center card-font-size">Orders<br /> <b>{adminOrders.length}</b></div>
                             </div>
@@ -86,7 +86,7 @@ export default function Dashboard () {
 
 
                     <div className="col-xl-3 col-sm-6 mb-3">
-                        <div className="card text-white bg-info o-hidden h-100">
+                        <div className="card text-white o-hidden h-100"  style={{ backgroundColor: '#B5927D' }}>
                             <div className="card-body">
                                 <div className="text-center card-font-size">Users<br /> <b>{users.length}</b></div>
                             </div>
@@ -101,7 +101,7 @@ export default function Dashboard () {
 
 
                     <div className="col-xl-3 col-sm-6 mb-3">
-                        <div className="card text-white bg-warning o-hidden h-100">
+                        <div className="card text-white o-hidden h-100" style={{ backgroundColor: '#B5927D' }}>
                             <div className="card-body">
                                 <div className="text-center card-font-size">Out of Stock<br /> <b>{outOfStock}</b></div>
                             </div>
