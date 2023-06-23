43a9a9b49c4d27a3edea5a58798a4d777c692f42
codesanta <jathurshan@protonmail.com>
2023-06-23 14:42:40 +0530

    xx

--- frontend/src/components/cart/OrderSuccess.js
+++ frontend/src/components/cart/OrderSuccess.js
@@ -5,7 +5,6 @@ export default function OrderSuccess() {
                 <img className="my-5 img-fluid d-block mx-auto" src="/images/success.png" alt="Order Success" width="200" height="200" />
 
                 <h2>Your Order has been placed successfully.</h2>
-
                 <a href="/orders">Go to Orders</a>
             </div>
 
